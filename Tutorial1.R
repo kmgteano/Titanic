67358b8b50c3cd1bd1f00f8231166ea2ec8fdd5d
kmgteano <kteano2006@my.fit.edu>
2015-05-16 17:59:34 -0400

    First decision tree implementation

--- Tutorial1.R
+++ Tutorial1.R
@@ -2,6 +2,8 @@
 # Titanic: Getting Started with R
 # Inspired by Trevor Stephens
 
+library(rpart)
+
 # Read in data sets
 train <- read.csv("train.csv")
 test <- read.csv("test.csv")
@@ -11,9 +13,16 @@ train_n <- length(train$PassengerId)
 test_n <- length(test$PassengerId)
 
 # Initialize: everyone dies. 
-test$Survived <- rep(0, test_n)
+test$Survived <- 0
 
+# Let's make a decision tree.
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data = train, method = "class")
+
+# Let's use the decision tree.
+
+Prediction <- predict(fit, test, type="class")
 
 ### Create CSV to submit
-submit <- data.frame(PassengerId = test$PassengerId, Survied = test$Survived)
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
 write.csv(submit, file = "submission.csv", row.names = FALSE)
